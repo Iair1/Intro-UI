9bd719fd4e75db176ea82d16269719c452f301b1
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-04-21 12:05:27 -0300

    Muestra saludo

---
+++ Assets/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI txtNombre;
+    public TMP_InputField inputNombre;
+    string nombreIngresado;
+    void Start()
+    {
+        txtNombre.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Saludar()
+    {
+        //obtener el input
+        nombreIngresado = inputNombre.text;
+        //concatenar nombre y saludo
+        txtNombre.text = "Hola " + nombreIngresado + "!";
+        //mostrar saludo
+    }
+}
