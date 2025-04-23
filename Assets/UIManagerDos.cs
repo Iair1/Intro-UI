3f5be6ada1b0e62710e15538aa20c940ce21a4bd
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-04-23 15:39:20 -0300

    ingresoClaveTerminado

---
+++ Assets/UIManagerDos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManagerDos : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMensaje;
+    string clave = "shakshuka";
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtMensaje.text = string.Empty;
+
+    }
+    public void ComprobarClave()
+    {
+        string claveIngresada = inputClave.text;
+        if(claveIngresada != clave)
+        {
+            txtMensaje.text = "Acceso denegado";
+            inputClave.text = "";
+        }
+        else
+        {
+            txtMensaje.text = "Bienvenido.";
+        }
+    }
+}
